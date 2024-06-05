a093884890bd7a003cb51a3124253bf10b34c4e6
ogzhanolguncu <ogzhan11@gmail.com>
2024-06-05 16:47:34 +0300

    fix: export llmclient

--- index.ts
+++ index.ts
@@ -5,3 +5,4 @@ export * from "./src/ratelimit";
 export * from "./src/error";
 export * from "./src/types";
 export { MODEL_NAME_WITH_PROVIDER_SPLITTER } from "./src/constants";
+export { type Model, UpstashLLMClient, UpstashLLMClientConfig } from "./src/upstash-llm-client";
