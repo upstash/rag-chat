7e128d658b8c1fca55e56ec3c3b317f8d17c0d7f
ogzhanolguncu <ogzhan11@gmail.com>
2024-06-27 17:20:46 +0300

    feat: add svelte example

---
+++ examples/sveltekit/src/routes/api/chat/+server.ts
@@ -0,0 +1,26 @@
+import { env } from "$env/dynamic/private";
+import type { Message } from "@ai-sdk/svelte";
+import { RAGChat, upstashModel } from "@upstash/rag-chat";
+import { aiUseChatAdapter } from "@upstash/rag-chat/nextjs";
+import { Index } from "@upstash/vector";
+import type { RequestHandler } from "./$types";
+
+export const POST = (async ({ request }) => {
+  const { messages } = await request.json();
+
+  const question = (messages as Message[]).at(-1)?.content;
+  if (!question) throw new Error("No question in the request");
+
+  const ragChat = new RAGChat({
+    vector: new Index({ token: env.UPSTASH_VECTOR_REST_TOKEN, url: env.UPSTASH_VECTOR_REST_URL }),
+    model: upstashModel("meta-llama/Meta-Llama-3-8B-Instruct", { apiKey: env.QSTASH_TOKEN }),
+  });
+  //OR
+  //   const ragChat = new RAGChat({
+  //     model: openaiModel("gpt-4"),
+  //   });
+
+  const response = await ragChat.chat(question, { streaming: true });
+
+  return aiUseChatAdapter(response);
+}) satisfies RequestHandler;
