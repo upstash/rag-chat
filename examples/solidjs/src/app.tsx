4a37b644d5d1f002e332ab608459cce208e08292
ogzhanolguncu <ogzhan11@gmail.com>
2024-06-28 11:46:28 +0300

    fix: formatter

--- examples/solidjs/src/app.tsx
+++ examples/solidjs/src/app.tsx
@@ -1,13 +1,13 @@
-import { Router } from '@solidjs/router';
-import { FileRoutes } from '@solidjs/start/router';
-import { Suspense } from 'solid-js';
+import { Router } from "@solidjs/router";
+import { FileRoutes } from "@solidjs/start/router";
+import { Suspense } from "solid-js";
 
-import './app.css';
+import "./app.css";
 
 export default function App() {
   return (
     <Router
-      root={props => (
+      root={(props) => (
         <>
           <Suspense>{props.children}</Suspense>
         </>
