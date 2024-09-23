156e144fe137264ed4099bcc12207ba4d294d678
ytkimirti <yusuftaha9@gmail.com>
2024-09-23 09:05:41 +0300

    chore: add comment on rsc import

--- src/nextjs/index.ts
+++ src/nextjs/index.ts
@@ -1,3 +1,7 @@
 export * from "./chat-adapter";
 export * from "./server-action-read-adapter";
+
+// This cant be exported here because you can't import ai/rsc
+// when not using a server component
+// Opposite is true for the read adapter, it can't be imported in the rsc-server.ts file
 export type * from "./server-action-write-adapter";
