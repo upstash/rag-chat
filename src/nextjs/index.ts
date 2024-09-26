ac7db4bd84592dc004f485cbe24eac20bb2b2411
ytkimirti <yusuftaha9@gmail.com>
2024-09-26 10:31:01 +0300

    chore: improve comment for rsc-server.ts

--- src/nextjs/index.ts
+++ src/nextjs/index.ts
@@ -1,7 +1,18 @@
 export * from "./chat-adapter";
 export * from "./server-action-read-adapter";
 
-// This cant be exported here because you can't import ai/rsc
-// when not using a server component
-// Opposite is true for the read adapter, it can't be imported in the rsc-server.ts file
+/*
+The reason we put export this in the rsc-server.ts file is because we can't import "ai/rsc" here.
+In ai/rsc, it has an export field like this
+
+"./rsc": {
+  "types": "./rsc/dist/index.d.ts",
+  "react-server": "./rsc/dist/rsc-server.mjs",
+  "import": "./rsc/dist/rsc-client.mjs"
+},
+
+Which means some of the functions in rsc-server.mjs can only be used when
+the used import key is "react-server". Or else we get this error
+> Attempted import error: 'createStreamableValue' is not exported from 'ai/rsc'
+*/
 export type * from "./server-action-write-adapter";
--- src/nextjs/rsc-server.ts
+++ src/nextjs/rsc-server.ts
@@ -1,2 +1,3 @@
+// Look at index.ts for the explanation
 export * from "./server-action-write-adapter";
 export * from "./chat-adapter";
